71b26929d70acf3535e8bb14b12c3255ab39a739
ntl <nathanladd+github@gmail.com>
2016-01-18 12:08:43 -0600

    Extract num-val file

--- lib/abnf/controls.rb
+++ lib/abnf/controls.rb
@@ -2,6 +2,7 @@ require 'abnf/controls/value'
 require 'abnf/controls/values'
 
 require 'abnf/controls/abnf'
+require 'abnf/controls/abnf/num_val'
 require 'abnf/controls/abnf/rfc_5234'
 require 'abnf/controls/elements'
 require 'abnf/controls/tokens'
