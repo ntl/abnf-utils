08cdcaf76a0465a0c1d5e8bfb3816c60b7879d6c
ntl <nathanladd+github@gmail.com>
2016-01-21 11:01:34 -0600

    Move repetition elements

--- lib/abnf/controls/elements.rb
+++ lib/abnf/controls/elements.rb
@@ -53,40 +53,6 @@ module ABNF
 
         Element::Repetition.new abnf, range, element
       end
-
-      module Repetition
-        def self.example
-          any_number
-        end
-
-        def self.any_number element=nil
-          element ||= Terminal.char_val
-
-          abnf = "#{Controls::ABNF::Repeat.any_number}#{element.abnf}"
-          range = Values::Repetition.any_number
-
-          Element::Repetition.new abnf, range, element
-        end
-
-        def self.fixed element=nil
-          element ||= Terminal.char_val
-
-          abnf = "#{Controls::ABNF::Repeat.fixed}#{element.abnf}"
-          count = Values::Repetition.fixed
-          range = (count..count)
-
-          Element::Repetition.new abnf, range, element
-        end
-
-        def self.bounded_range element=nil
-          element ||= Terminal.char_val
-
-          abnf = "#{Controls::ABNF::Repeat.bounded_range}#{element.abnf}"
-          range = Values::Repetition.bounded_range
-
-          Element::Repetition.new abnf, range, element
-        end
-      end
     end
   end
 end
